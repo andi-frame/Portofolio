c51e31f6f0578779212ae26cd36a6cda8a7b370c
andi-frame <andifarhan1094@gmail.com>
2024-08-10 23:03:26 +0700

    feat: refine landing page and add profile pict

--- src/app/_components/Landing.tsx
+++ src/app/_components/Landing.tsx
@@ -1,26 +1,36 @@
 import React from "react";
 
+import Image from "next/image";
+
 const Landing = () => {
   return (
-    <div className="w-max-screen flex h-screen flex-col gap-6 bg-white px-20 pt-20">
-      <div className="w-2/5 text-black">
-        <div className="text-4xl font-bold">
-          I&apos;M ANDI FARHAN HIDAYAT
-        </div>
+    <div className="w-full flex h-fit gap-6 bg-white px-32 pt-20">
+      <div className="text-black w:4/5 pt-5">
+        <div className="text-4xl font-bold">I&apos;M ANDI FARHAN HIDAYAT</div>
         <div className="text-2xl font-semibold">Software Engineer</div>
         <div>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
-          non quo repellat exercitationem delectus sint rerum blanditiis.
-          Aliquam, doloremque commodi!
+          Undergraduate student of informatics engineering highly dedicated to
+          always learn, explore, and develop his skills. Open to collaborate,
+          learn, and develop with others. Eager to help others and solve
+          challenging problems.
+        </div>
+        <div className="item-center mt-5 flex justify-start gap-16 pl-12 text-black">
+          <div className="border-lg flex h-8 w-40 items-center justify-center text-lg font-medium text-gray-900 ring-2 ring-black hover:font-bold">
+            View Projects
+          </div>
+          <div className="border-lg flex h-8 w-40 items-center justify-center text-lg font-medium text-gray-900 ring-2 ring-black hover:font-bold">
+            Reach Me Out
+          </div>
         </div>
       </div>
-      <div className="item-center flex w-2/5 justify-start gap-16 pl-12 text-black">
-        <div className="border-lg flex h-8 w-40 items-center justify-center bg-gray-400 text-lg font-bold text-gray-900 ring-2 ring-black">
-          View Projects
-        </div>
-        <div className="border-lg flex h-8 w-40 items-center justify-center bg-gray-400 text-lg font-bold text-gray-900 ring-2 ring-black">
-          Reach Me Out
-        </div>
+      <div className="flex h-fit w-full px-2 justify-start">
+        <Image
+          src="/Profile2.jpg"
+          width={250}
+          height={250}
+          alt="Picture of the author"
+          className="rounded-full"
+        />
       </div>
     </div>
   );
