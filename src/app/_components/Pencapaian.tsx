a804b4647e29e94830dc918e864e1efe318aa103
andi-frame <andifarhan1094@gmail.com>
2024-08-10 23:43:27 +0700

    Feat: add pencapaian

---
+++ src/app/_components/Pencapaian.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+
+const Pencapaian = () => {
+  return (
+    <div className="h-fit w-full bg-gray-100 px-32 py-5">
+      <div className="flex flex-col gap-2">
+        <span className="text-2xl font-bold">Honors and Awards</span>
+        <hr />
+        <div className="">
+          <div className="text-lg font-semibold">
+            1st Place - Battle of Technology - IT Venture
+          </div>
+          <div className="">
+            Issued by Himpunan Mahasiswa Informatika Universitas Sebelas Maret ·
+            Nov 2022
+          </div>
+          <div className="py-2">
+            Battle of Technology (ВоТ) is one of the branches of the competition
+            from a series of Pingfest 2022 events organized by the Informatics
+            Student Association of the Faculty of Information Technology and
+            Data Science, Sebelas Maret University with the aim of testing
+            participants&apos; abilities and understanding of Computer Science
+            and Programming.
+          </div>
+        </div>
+        <hr />
+        <div className="">
+          <div className="text-lg font-semibold">
+            Finalist - Olimpiade Penelitian Siswa Indonesia (OPSI)
+          </div>
+          <div className="">
+            Issued by Kementerian Pendidikan dan Kebudayaan (Ministry of
+            Education and Culture Republic of Indonesia) · Nov 2022
+          </div>
+          <div className="py-2">
+            OPSI is a place for high school/MA/equivalent students to actualize
+            their talents, interests and abilities in research and innovation as
+            well as instilling a research culture among students. This activity
+            is also a selection of superior research works to be included in
+            various research competitions, scientific forums, and national and
+            international publications.
+          </div>
+        </div>
+        <hr />
+        <div className="">
+          <div className="text-lg font-semibold">
+            Finalist - Schematics National Logic Competition
+          </div>
+          <div className="">
+            Issued by Himpunan Mahasiswa Teknik Computer-Informatika Institut
+            Teknologi Sepuluh Nopember · Sep 2022
+          </div>
+          <div className="py-2">
+            Schematics National Logic Competition 2022 is a competition that
+            tests the ability to think logically and analytically through
+            various interesting problems and games.
+          </div>
+        </div>
+        <hr />
+      </div>
+    </div>
+  );
+};
+
+export default Pencapaian;
