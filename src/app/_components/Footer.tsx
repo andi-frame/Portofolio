064b9ee3e0abb836ea9d7c85b705f3c0f20aa6bc
andi-frame <andifarhan1094@gmail.com>
2024-08-10 23:42:58 +0700

    Feat: add footer

---
+++ src/app/_components/Footer.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="flex h-fit w-full items-center justify-start bg-gray-200 py-5 px-32">
+      <div className="w-3/5">
+        <span className="text-lg font-bold">Contact</span>
+        <div>Email: andifarhan1094@gmail.com</div>
+        <div>
+          <a href="https://www.linkedin.com/in/andi-farhan-hidayat/">
+            Linkedin: Andi Farhan Hidayat
+          </a>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
