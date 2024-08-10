73e28ae23b48bc718b8a48b0b94ecccbd9036d2a
andi-frame <andifarhan1094@gmail.com>
2024-08-10 23:43:10 +0700

    Feat: add experience

---
+++ src/app/_components/Experience.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+
+const Experience = () => {
+  return (
+    <div className="h-fit w-full px-32 py-5">
+      <div className="flex flex-col gap-2">
+        <span className="text-2xl font-bold">Experience</span>
+        <hr />
+        <div className="">
+          <div className="text-lg font-semibold">Back-End Social IT OSKM</div>
+          <div className="">OSKM ITB</div>
+          <div className="font-light text-gray-400">
+            July 2024 - Present · 1 mos
+          </div>
+          <div className="font-light text-gray-400">
+            Bandung, West Java, Indonesia · Hybrid
+          </div>
+        </div>
+        <hr />
+        <div className="">
+          <div className="text-lg font-semibold">
+            Robotics Software Control Department
+          </div>
+          <div className="">Aksantara ITB</div>
+          <div className="font-light text-gray-400">
+            Feb 2024 - Present · 7 mos
+          </div>
+          <div className="font-light text-gray-400">
+            Bandung, West Java, Indonesia · Hybrid
+          </div>
+          <div className="py-2">Team: Vertical Take-Off and Landing (VTOL)</div>
+        </div>
+        <hr />
+        <div className="">
+          <div className="text-lg font-semibold">Web Developer </div>
+          <div className="">IMPACT ITB</div>
+          <div className="font-light text-gray-400">
+            May 2024 - Jul 2024 · 3 mos
+          </div>
+          <div className="font-light text-gray-400">
+            Bandung, West Java, Indonesia · Hybrid
+          </div>
+        </div>
+        <hr />
+        <div className="">
+          <div className="text-lg font-semibold">
+            Head of Front-End Sub Division Aksi Angkatan STEIK&apos;23
+          </div>
+          <div className="">
+            Sekolah Teknik Elektro dan Informatika - Komputasi (STEI-K) ITB 2023
+          </div>
+          <div className="font-light text-gray-400">
+            Sep 2023 - Oct 2023 · 2 mos
+          </div>
+          <div className="font-light text-gray-400">Remote</div>
+        </div>
+        <hr />
+      </div>
+    </div>
+  );
+};
+
+export default Experience;
